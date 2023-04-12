f16cc3b7dae6d51ac44de00653c35a277b83e226
Suzan Dsouza <suzandsouza1505@gmail.com>
2023-04-12 10:29:07 +0530

    file uploads

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const path=require('path')
 const express=require('express')
+const multer = require('multer');
+const zlib = require('zlib');
+const crypto = require('crypto');
+const fs = require('fs');
 const dotenv=require('dotenv').config()
 const port=process.env.PORT||5000
 const app=express()
@@ -8,32 +12,66 @@ const app=express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set static folder
-app.get('/api/enccomp',(req,res)=>{
-   
+// Define the directory to which files will be saved
+const uploadDirectory = './uploads';
+
+// Set up multer for file upload handling
+const storage = multer.diskStorage({
+  destination: uploadDirectory,
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const fileFilter = (req, file, cb) => {
+    // Check the file extension
+    if (file.originalname.endsWith('.pdf')) {
+      cb(null, true); // Accept the file
+    } else {
+      cb(new Error('File type not allowed!'), false); // Reject the file
+    }
+  };
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter
+});
+
+app.post('/upload',upload.single('file'),(req,res)=>{
+    const file = req.file.buffer;
+  const password = req.body.password;
+
+  // Compress the file
+  zlib.deflate(file, (err, compressed) => {
+    if (err) {
+        console.log('defalte mai error tha')
+      return res.status(500).send('Failed to compress file');
+    }
+
+    // Encrypt the compressed file
+    const algorithm = 'aes-256-cbc';
+    const key = crypto.scryptSync(password, 'salt', 32);
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    let encrypted = cipher.update(compressed);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    // Send the encrypted file to the client
+    try{
+        res.set({
+      'Content-Type': 'application/octet-stream',
+      'Content-Disposition': 'attachment; filename="encrypted.compressed"'
+    });
+    res.send(Buffer.concat([iv, encrypted]));
+}
+catch(e){
+    console.log('idhar tha error')
+}
+  });
 })
+// Set static folder
+// app.get('/api/enccomp',(req,res)=>{
+   
+// })
 app.use("/api/home",express.static(path.join(__dirname, 'frontend/public')));
-
-
-// const fs = require('fs');
-// const zlib = require('zlib');
-// const crypto = require('crypto');
-// // Middleware for compressing and encrypting files
-// app.use("/api",(req, res, next) => {
-//   const filePath = '/path/to/file.txt'; // Replace with the path to your file
-//   const fileContents = fs.createReadStream(filePath);
-//   const gzip = zlib.createGzip();
-//   const cipher = crypto.createCipher('aes-256-cbc', 'mysecretkey');
-
-//   res.set({
-//     'Content-Type': 'application/octet-stream',
-//     'Content-Disposition': 'attachment; filename="compressed-encrypted-file.bin"',
-//     'Content-Encoding': 'gzip',
-//   });
-
-//   fileContents.pipe(gzip).pipe(cipher).pipe(res);
-// });
-
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
